dab0d73b8794c4a9e49593fa13072c659b91d94e
papaj10211 <papaj10211@f64ab82b-a82b-0410-9551-fd484432d37d>
2007-03-16 14:35:25 +0000

    Repaired XmlReaderWriter and in Tile filed tileType changed to public

--- trunk/AntHill.NET/XmlReaderWriter.cs
+++ trunk/AntHill.NET/XmlReaderWriter.cs
@@ -13,32 +13,9 @@ namespace AntHill.NET
 
     class XmlReaderWriter
     {
+        
         List<string> Errors = new List<string>();
-        int rowCount;
-        int colCount;
-        List<string> MapsRows = new List<string>();
-        int maxLife;
-        int maxLifeWithoutFood;
-        int turnNumberToBecomeHungry;
-        int strength;
-        int forgettingTime;
-        int sightRadius;
-        int startCountWarrior;
-        int startCountWorker;
-        int layEggProbability;
-        int xPosition;
-        int yPosition;
-        int hatchWarriorProbability;
-        int hatchTime;
-        int maxHealth;
-        int probabilitySpider;
-        int foodQuantityAfterDeath;
-        int cloudWidth;
-        int probabilityRain;
-        int maxDuration;
-        int probabilityFood;
-        int lifeTime;
-        int radius;
+      
 
         bool ValidateMe()
         {
@@ -73,7 +50,9 @@ namespace AntHill.NET
 
         public void ReadMe()
         {
-            XmlTextReader textReader = new XmlTextReader("C:\\b.xml");
+            int k=0;
+            int l=0;
+            XmlTextReader textReader = new XmlTextReader("AntHill.Config.xml");
             textReader.Read();
             while (textReader.Read())
             {
@@ -82,61 +61,81 @@ namespace AntHill.NET
                     {
                         case "WorldMap":
                             {
-                                rowCount = int.Parse(textReader.GetAttribute("rowCount"));
-                                colCount = int.Parse(textReader.GetAttribute("colCount"));
+                                AntHillConfig.mapRowCount= int.Parse(textReader.GetAttribute("rowCount"));
+                                AntHillConfig.mapColCount= int.Parse(textReader.GetAttribute("colCount"));
                             } break;
                         case "Map":
-                            {
-                                MapsRows.Add(textReader.GetAttribute("row"));
+                            {   
+                                
+                                string s=(textReader.GetAttribute("row"));
+                                CharEnumerator iter=s.GetEnumerator();
+                                while (iter.MoveNext())
+                                {
+                                    switch (iter.Current)
+                                    {
+                                        case 'o':
+                                            AntHillConfig.map[k][l].tileType= TileType.Indoor;
+                                            break;
+                                        case 'x':
+                                            AntHillConfig.map[k][l].tileType = TileType.Wall;
+                                            break;
+                                        case 's':
+                                            AntHillConfig.map[k][l].tileType = TileType.Outdoor;
+                                            
+                                            break;
+                                    }
+                                    l++;
+                                 }
+                                 k++;
                             } break;
                         case "Ant":
                             {
-                                maxLife = int.Parse(textReader.GetAttribute("maxLife"));
-                                maxLifeWithoutFood = int.Parse(textReader.GetAttribute("maxLifeWithoutFood"));
-                                turnNumberToBecomeHungry = int.Parse(textReader.GetAttribute("turnNumberToBecomeHungry"));
-                                strength = int.Parse(textReader.GetAttribute("strength"));
-                                forgettingTime = int.Parse(textReader.GetAttribute("forgettingTime"));
-                                sightRadius = int.Parse(textReader.GetAttribute("sightRadius"));
+                                AntHillConfig.antMaxLife= int.Parse(textReader.GetAttribute("maxLife"));
+                                AntHillConfig.antMaxLifeWithoutFood = int.Parse(textReader.GetAttribute("maxLifeWithoutFood"));
+                                AntHillConfig.antTurnNumberToBecomeHungry= int.Parse(textReader.GetAttribute("turnNumberToBecomeHungry"));
+                                AntHillConfig.antStrength = int.Parse(textReader.GetAttribute("strength"));
+                                AntHillConfig.antForgettingTime= int.Parse(textReader.GetAttribute("forgettingTime"));
+                                AntHillConfig.antSightRadius= int.Parse(textReader.GetAttribute("sightRadius"));
                             } break;
                         case "Warrior":
                             {
-                                startCountWarrior = int.Parse(textReader.GetAttribute("startCount"));
+                                AntHillConfig.warriorStartCount= int.Parse(textReader.GetAttribute("startCount"));
                             } break;
                         case "Worker":
                             {
-                                startCountWorker = int.Parse(textReader.GetAttribute("startCount"));
+                                AntHillConfig.workerStartCount= int.Parse(textReader.GetAttribute("startCount"));
                             } break;
                         case "Queen":
                             {
-                                layEggProbability = int.Parse(textReader.GetAttribute("layEggProbability"));
-                                xPosition = int.Parse(textReader.GetAttribute("xPosition"));
-                                yPosition = int.Parse(textReader.GetAttribute("yPosition"));
+                                AntHillConfig.queenLayEggProbability = int.Parse(textReader.GetAttribute("layEggProbability"));
+                                AntHillConfig.queenXPosition= int.Parse(textReader.GetAttribute("xPosition"));
+                                AntHillConfig.queenYPosition= int.Parse(textReader.GetAttribute("yPosition"));
                             } break;
                         case "Egg":
                             {
-                                hatchWarriorProbability = int.Parse(textReader.GetAttribute("hatchWarriorProbability"));
-                                hatchTime = int.Parse(textReader.GetAttribute("hatchTime"));
+                                AntHillConfig.eggHatchWarriorProbability = int.Parse(textReader.GetAttribute("hatchWarriorProbability"));
+                                AntHillConfig.eggHatchTime = int.Parse(textReader.GetAttribute("hatchTime"));
                             } break;
                         case "Spider":
                             {
-                                maxHealth = int.Parse(textReader.GetAttribute("maxHealth"));
-                                probabilitySpider = int.Parse(textReader.GetAttribute("probability"));
-                                foodQuantityAfterDeath = int.Parse(textReader.GetAttribute("foodQuantityAfterDeath"));
+                                AntHillConfig.spiderMaxHealth= int.Parse(textReader.GetAttribute("maxHealth"));
+                                AntHillConfig.spiderProbability = int.Parse(textReader.GetAttribute("probability"));
+                                AntHillConfig.spiderFoodQuantityAfterDeath = int.Parse(textReader.GetAttribute("foodQuantityAfterDeath"));
                             } break;
                         case "Rain":
                             {
-                                cloudWidth = int.Parse(textReader.GetAttribute("cloudWidth"));
-                                probabilityRain = int.Parse(textReader.GetAttribute("probability"));
-                                maxDuration = int.Parse(textReader.GetAttribute("maxDuration"));
+                                AntHillConfig.rainWidth= int.Parse(textReader.GetAttribute("cloudWidth"));
+                                AntHillConfig.rainProbability = int.Parse(textReader.GetAttribute("probability"));
+                                AntHillConfig.rainMaxDuration= int.Parse(textReader.GetAttribute("maxDuration"));
                             } break;
                         case "Food":
                             {
-                                probabilityFood = int.Parse(textReader.GetAttribute("probability"));
+                               AntHillConfig.foodProbability = int.Parse(textReader.GetAttribute("probability"));
                             } break;
                         case "Signal":
                             {
-                                lifeTime = int.Parse(textReader.GetAttribute("lifeTime"));
-                                radius = int.Parse(textReader.GetAttribute("radius"));
+                                AntHillConfig.messageLifeTime = int.Parse(textReader.GetAttribute("lifeTime"));
+                                AntHillConfig.messageRadius= int.Parse(textReader.GetAttribute("radius"));
                             } break;
 
                     }
